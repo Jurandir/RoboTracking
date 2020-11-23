0cf750d6523ee55f230c02b9b950d71d7a2bf9e7
Jurandir Brito Ferreira Junior <jurandir.junior@termaco.com.br>
2020-11-23 15:09:23 -0300

    Conclusão das ocorrências - Fase do Manifesto

---
+++ controllers/geraOcorrenciaSaidaDoCD.js
@@ -0,0 +1,31 @@
+const sqlExec       = require('../connection/sqlExec')
+const sendLog       = require('../helpers/sendLog')
+
+const fs            = require('fs')
+const path          = require('path')
+const sqlFileName   =  path.join(__dirname, '../sql/rotinas/OCORRENCIA_SAIDA_DO_CD.sql')
+
+var sqlRegistraMan = fs.readFileSync(sqlFileName, "utf8")
+
+async function geraOcorrenciaSaidaDoCD() {    
+    let dados = {}
+
+    let sql = sqlRegistraMan
+ 
+    try {
+        result = await sqlExec(sql)    
+        
+        if (result.rowsAffected==-1){
+            throw new Error(`DB ERRO - ${result.Erro} : SQL => [ ${sql} ]`)
+        }
+              
+        return result
+  
+    } catch (err) {
+        dados = { "erro" : err.message, "rotina" : "geraOcorrenciaSaidaDoCD", "sql" : sql, rowsAffected: -1 }
+        sendLog('ERRO', JSON.stringify(dados) )
+        return dados
+    } 
+}
+
+module.exports = geraOcorrenciaSaidaDoCD
--- webhook.js
+++ webhook.js
@@ -4,29 +4,41 @@ const colors = require('colors')
 
 require('dotenv').config()
 
-const displayDados                  = require('./helpers/displayDados')
-const getCliente                    = require('./controllers/getCliente')
-const checkNovasNFs                 = require('./controllers/checkNovasNFs')
-const registraCargaNF               = require('./controllers/registraCargaNF')
-const getNFsNaoValidadas            = require('./controllers/getNFsNaoValidadas')
-const getCargaAPIitrack             = require('./controllers/getCargaAPIitrack')
-const geraOcorrenciasIniciais       = require('./controllers/geraOcorrenciasIniciais')
-const getToken                      = require('./controllers/getToken')
-const sendLog                       = require('./helpers/sendLog')
+const displayDados                    = require('./helpers/displayDados')
+const getCliente                      = require('./controllers/getCliente')
+const checkNovasNFs                   = require('./controllers/checkNovasNFs')
+const registraCargaNF                 = require('./controllers/registraCargaNF')
+const registraManifestoNF             = require('./controllers/registraManifestoNF')
+const atualizaDataManifestoNF         = require('./controllers/atualizaDataManifestoNF')
+const atualizaDataBaixaManifestoNF    = require('./controllers/atualizaDataBaixaManifestoNF')
+const geraOcorrenciaChegadaFilDestino = require('./controllers/geraOcorrenciaChegadaFilDestino')
+const geraOcorrenciaSaidaDoCD         = require('./controllers/geraOcorrenciaSaidaDoCD')
+const getNFsNaoValidadas              = require('./controllers/getNFsNaoValidadas')
+const getCargaAPIitrack               = require('./controllers/getCargaAPIitrack')
+const geraOcorrenciasIniciais         = require('./controllers/geraOcorrenciasIniciais')
+const getToken                        = require('./controllers/getToken')
+const sendLog                         = require('./helpers/sendLog')
 
 
 const check_time                 = process.env.CHECK_TIME      || 10000   // mseg 
 const time_evidencias            = process.env.TIME_EVIDENCIAS || 1800000 // mseg 
 const node_env                   = process.env.NODE_ENV        || 'Test' 
-let sucesso                      = false
-let id                           = 0
-let token                        = ''
-let checks                       = 0
-let notasFiscaisIncluidas        = 0
-let ocorrenciasIniciaisIncluidas = 0
-let x_botCheckNovasNFs           = 0
-let x_botGeraOcorrenciasIniciais = 0
-let x_botGetNFsNaoValidadas      = 0
+
+let sucesso                              = false
+let id                                   = 0
+let token                                = ''
+let checks                               = 0
+let notasFiscaisIncluidas                = 0
+let ocorrenciasIniciaisIncluidas         = 0
+let novosManifestosRegistrados           = 0
+let novasOcorrenciaSaidaDoCD             = 0
+let novasOcorrenciaChegadaFilial         = 0
+let x_botCheckNovasNFs                   = 0
+let x_botGeraOcorrenciasIniciais         = 0
+let x_botGetNFsNaoValidadas              = 0
+let x_botRegistraManifestoNF             = 0
+let x_botGeraOcorrenciaSaidaDoCD         = 0
+let x_botGeraOcorrenciaChegadaFilDestino = 0
 
 // Tela inicial
 process.stdout.write('\x1B[2J\x1B[0f')
@@ -79,7 +91,7 @@ async function botCheckNovasNFs() {
       checkNovasNFs().then((ret)=>{
          if(ret.rowsAffected>0){
             notasFiscaisIncluidas++
-            sendLog('SUCESSO',`Notas Fiscais - Incluidas (${ret.rowsAffected})`)
+            sendLog('AVISO',`Notas Fiscais - Incluidas (${ret.rowsAffected})`)
          }
          x_botCheckNovasNFs += ret.rowsAffected
       })
@@ -91,11 +103,49 @@ async function botGeraOcorrenciasIniciais() {
       geraOcorrenciasIniciais().then((ret)=>{
          if(ret.rowsAffected>0){
             ocorrenciasIniciaisIncluidas++
-            sendLog('SUCESSO',`Ocorrências Iniciais - Incluidas (${ret.rowsAffected})`)
+            sendLog('AVISO',`Ocorrências Iniciais - Incluidas (${ret.rowsAffected})`)
          }
          x_botGeraOcorrenciasIniciais +=  ret.rowsAffected
       })      
       setTimeout(botGeraOcorrenciasIniciais,check_time)
+}
+
+// Registra Manifesto na NF
+async function botRegistraManifestoNF() {
+   registraManifestoNF().then((ret)=>{
+      if(ret.rowsAffected>0){
+         novosManifestosRegistrados++
+         sendLog('AVISO',`Registro de novos manifestos - (${ret.rowsAffected})`)
+      }
+      x_botRegistraManifestoNF +=  ret.rowsAffected
+   })      
+   setTimeout(botRegistraManifestoNF,check_time)
+}
+
+// Gera ocorrências de saida do CD
+async function botGeraOcorrenciaSaidaDoCD() {
+   await geraOcorrenciaSaidaDoCD().then((ret)=>{
+      if(ret.rowsAffected>0){
+         novasOcorrenciaSaidaDoCD++
+         sendLog('AVISO',`Manifestos - Ocorrências de saída - (${ret.rowsAffected})`)
+      }
+      x_botGeraOcorrenciaSaidaDoCD +=  ret.rowsAffected
+   })      
+   await atualizaDataManifestoNF()
+   setTimeout(botGeraOcorrenciaSaidaDoCD,check_time)
+}
+
+// Gera ocorrências - "CHEGADA NA CIDADE OU FILIAL DE DESTINO"
+async function botGeraOcorrenciaChegadaFilDestino() {
+   await geraOcorrenciaChegadaFilDestino().then((ret)=>{
+      if(ret.rowsAffected>0){
+         novasOcorrenciaChegadaFilial++
+         sendLog('AVISO',`Baixa - Ocorrências de Chegada Filial destino - (${ret.rowsAffected})`)
+      }
+      x_botGeraOcorrenciaChegadaFilDestino +=  ret.rowsAffected
+   })      
+   await atualizaDataBaixaManifestoNF()
+   setTimeout(botGeraOcorrenciaChegadaFilDestino,check_time)
 }
 
 // Valida na API a NF para o destinatario
@@ -113,6 +163,7 @@ async function botGetNFsNaoValidadas() {
             let isErr          = ret.isErr
             if(( count==0) || (isErr==true) || (isAxiosError==true)) {
                sendLog('WARNING',`(Validação Falhou) - Destinatário: ${cnpj}, NF: ${nroFiscal}, NÃO OK, Erro: ${isErr},AxiosErro: ${isAxiosError} .`)
+               registraCargaNF(nota.DANFE,0)
                return
             }
             let danfe     = ret.dados.data.list[0].carga.danfe
@@ -136,5 +187,8 @@ async function monitorarOcorrencias() {
       botCheckNovasNFs()
       botGeraOcorrenciasIniciais()
       botGetNFsNaoValidadas()
+      botRegistraManifestoNF()
+      botGeraOcorrenciaSaidaDoCD()
+      botGeraOcorrenciaChegadaFilDestino()
       displayStatistics()
 }

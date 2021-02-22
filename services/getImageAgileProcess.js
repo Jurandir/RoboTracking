9b49186d300182cfeda1624f1d13d67a429a5244
Jurandir Brito Ferreira Junior <jurandir.junior@termaco.com.br>
2021-02-22 17:57:24 -0300

    Ajuste imagem AgileProcess

--- services/getImageAgileProcess.js
+++ services/getImageAgileProcess.js
@@ -6,9 +6,21 @@ const endpoint = '/service'
 const server =  process.env.AGILE_REST_URL || 'http://termaco.agileprocess.com.br/dash-beta/api/v2'
 
 const getImageAgileProcess = async (doc) => {
-    let codeAgile = doc.substring(0,3)+'-'+doc.substring(3,4)+'-'+doc.substring(4,14)
+    let codeAgile = doc.substring(0,3)+'-'+doc.substring(3,4)+'-'+doc.substring(4,20)
     let params = agileBody(codeAgile)
-    return await loadAPI(method,endpoint,server,params)
+
+    console.log('codeAgile',codeAgile,params,params.filters.logically_deleted)
+    let ret    = await loadAPI(method,endpoint,server,params)
+
+
+    if(ret.json_response==null) {
+        let new_param = params
+        let deletad   = !params.filters.logically_deleted
+        new_param.filters.logically_deleted = deletad
+        console.log('codeAgile',codeAgile,new_param, deletad)
+        ret    = await loadAPI(method,endpoint,server,new_param)
+    }
+    return ret
 }
 
 module.exports = getImageAgileProcess

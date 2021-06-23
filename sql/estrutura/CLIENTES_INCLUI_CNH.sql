b9643484020d3e1d8c7820c3f56ff98a403f82a1
Jurandir Brito Ferreira Junior <jurandir.junior@termaco.com.br>
2021-06-23 15:40:40 -0300

    Fim versão anterior

---
+++ sql/estrutura/CLIENTES_INCLUI_CNH.sql
@@ -0,0 +1,27 @@
+USE [SIC]
+GO
+
+INSERT INTO [dbo].[CLIENTES]
+           ([CNPJ_CLI]
+           ,[SERVIDOR]
+           ,[LOGIN]
+           ,[SENHA]
+           ,[URL_OCORRENCIA]
+           ,[URL_OCORRENCIA_SIMPLIFICADA]
+           ,[URL_OCORRENCIA_CLIENTE]
+           ,[URL_EVIDENCIAR_OCORRENCIA]
+           ,[CHAVE_PUBLICA]
+           ,[REF_LAYOUT])
+     VALUES
+           ('01844555'
+           ,'https://www.itrackbrasil.com.br/ws'
+           ,'11552312000710'
+           ,'TERMACO789'
+           ,''
+           ,''
+           ,''
+           ,''
+           ,''
+           ,2)
+
+GO
